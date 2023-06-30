30bbb7b1f34c1bf09c1e0e3458204981a624db88
AxelSalim <adjakidjememiaghe@gmail.com>
2023-06-30 18:01:32 +0100

    Arkanoid

--- main.js
+++ main.js
@@ -1,144 +1,174 @@
 'use strict'
-// debugger
-let width = window.innerWidth - 100;
-console.log(width);
-let height = window.innerHeight - 50;
-console.log(height);
 
-let ball = {
-    x:(width - 40) / 2,
-    y:(height - 40) - 30,
-    color: "red",
-    radius: 40,
-    directionX: 5,
-    directionY :5,
+let circle = {
+    color: "Tomato",
+    radius: 20,
+    x: 450,
+    y: 660
 };
 
-let game = {
-    width: width,
-    height: height,
-    color: "#ccc",
-    gameOver: false,
-};
-
-let paddle ={
-    
-    speed: 1,
-    color: "blue",
+let paddel = {
+    positX: 400,
+    positY: 700,
+    vitesse: 20,
+    color: "MediumSeaGreen",
     width: 100,
-    height: 30,
-    direction: 4,
-    x: ((width - 40) - 100) / 2,
-    y: (height - 40) + 10 , 
+    height: 20,
+    direction: 1
 };
 
-let canva;
-let context;
-let request;
-function playGame(){
-    // if(ball.y - ball.radius < 0 ){
-    //     ball.directionY *= -1;
-    // }else if(ball.y + ball.radius > game.height ){
-    //     ball.directionY *= -1;
-    // };
-    // ball.y = ball.y - ball.directionX;  
-    // if(ball.x - ball.radius < 0 ){
-    //     ball.directionX *= -1;
-    // }else if(ball.x + ball.radius > game.height ){
-    //     ball.directionX *= -1;
-    // };
-    // ball.x = ball.x - ball.directionX;  
-    detectColision();  
-    if(ball.y + ball.radius > game.height){
-        ball.directionY *= -1;
-        game.gameOver = true;        
-        // ball.directionY = 0;
-        context.font = 'bold 100px cursive';
-        context.fillStyle = "black";
-        context.fillText("GAME OVER 🎮", (width / 2) - 300, height / 2 );
-        cancelAnimationFrame(requestId);
-        
-    }else if(ball.y - ball.radius < 0 ){
-        ball.directionY *= -1;
-    };
-    ball.y = ball.y + ball.directionY;
-    
-    if(ball.x + ball.radius > game.width  ){
-        ball.directionX *= -1;
-    }else if(ball.x - ball.radius < 0 ){
-        ball.directionX *= -1;
-    };
-    ball.x = ball.x + ball.directionX; 
-    displayball();
-    let requestId = requestAnimationFrame(playGame);
-    request = requestId;
-};  
+let canvas, ctx;
+let keyboardEvent, stopanimate, SpaceCount = 0;
+let gameOver = false;
+let directionX = 1, directionY = 1;
 
-function displayball(){
-    context.clearRect(0, 0, canva.width, canva.height)
-    context.fillStyle = ball.color;
-    context.beginPath();
-    context.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
-    context.fill();
-    displayPaddle();
-};
+//Chargement du DOM
+document.addEventListener("DOMContentLoaded", () => {
 
-function displayPaddle(){
-    context.fillStyle = paddle.color;
-    context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
-}
-    
-function initGame(){
-    document.addEventListener("keydown" , function(event){
-        let spaceCount = 0;
-        console.log(event.key);
-        if(event.key === "ArrowRight" && paddle.x + paddle.width < game.width){
-            paddle.x += 20;
-        }else if(event.key ==="ArrowLeft" && paddle.x > 0){
-            paddle.x -= 20;
-        }else if(event.key === " "){
-            spaceCount++;
-            if(spaceCount % 2 === 0){
-                playGame();
-            }else if(spaceCount % 2 === 1){
-                cancelAnimationFrame(request);
-            };
-        };
-    });                
-};
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
 
-function detectColision(){
-    
-
-    if((ball.x - ball.radius) >= paddle.x - (ball.radius * 2) && (ball.x + ball.radius) <= (paddle.x + paddle.width) + 
-    (ball.radius * 2) && (ball.y + ball.radius) >= paddle.y){
-        ball.directionY *= -1;
-        ball.y = ball.y + ball.directionY;
-    };
-};
-
-function gameOver(){
-    if(game.gameOver === true){
-        ball.directionY = 0;
-        context.font = 'bold 40px cursive';
-        context.fillStyle = "black";
-        context.fillText("GAME OVER 🎮", width / 2, height / 2 );
-        cancelAnimationFrame(requestId);
-    };
-};
-
-document.addEventListener("DOMContentLoaded", function(){    
-    
-    canva = document.getElementById('canvas');
-    canva.width = game.width;
-    canva.height = game.height;
-    canva.style.border = "1px solid black";
-    canva.style.backgroundColor = game.color;
-    context = canva.getContext('2d');
-
-    displayball();
-    displayPaddle();
     playGame();
-    initGame();    
-    
+    initGame();
+
 });
+
+
+
+/**
+ * Mouvement de la Balle (↓ ↑)
+ */
+function playGame() {
+
+    circle.y -= directionY * 5;
+    if (circle.y >= canvas.height - circle.radius || circle.y - circle.radius <= 0) {
+        directionY = -directionY;
+    }
+
+    stopanimate = requestAnimationFrame(playGame);
+    detectCollisions();
+    displayGame();
+};
+
+
+
+/**
+ * Mouvement du Paddel (← →)
+ */
+function initGame() {
+    keyboardEvent = document.addEventListener("keydown", (event) => {
+
+        if (event.key === " ") {
+            SpaceCount++;
+
+            console.log(SpaceCount);
+            if ((SpaceCount % 2) === 0) {
+                playGame();
+            }
+            else if ((SpaceCount % 2) === 1) {
+                cancelAnimationFrame(stopanimate);
+            }
+
+            if (gameOver === true) {
+                initPositions();
+                gameOver = false;
+                console.log(gameOver);
+                console.log(SpaceCount);
+            }
+
+        }
+
+        switch (event.key) {
+            case "ArrowRight": // →
+                if (paddel.positX + paddel.width < canvas.width) {
+                    paddel.direction = 1
+                    paddel.positX += paddel.direction * paddel.vitesse;
+                }
+                break;
+            case "ArrowLeft": // ←
+                if (paddel.positX > 0) {
+                    paddel.direction = -1;
+                    paddel.positX += paddel.direction * paddel.vitesse;
+                }
+                break;
+        }
+
+    });
+
+};
+
+
+
+/**
+ * Collision de la balle et du panel
+ */
+function detectCollisions() {
+    if (circle.y >= (paddel.positY - paddel.height - circle.radius) && (circle.x <= paddel.positX + paddel.width && circle.x >= paddel.positX)) {
+        directionY = -directionY;
+    }
+    else if (circle.y >= canvas.height - circle.radius) {
+        gameOver = true;
+    }
+};
+
+
+
+
+/**
+ * Création de la Balle
+ */
+function displayGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#ddd";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.beginPath();
+    ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+    ctx.fillStyle = circle.color;
+    ctx.fill();
+
+
+    ctx.fillStyle = paddel.color;
+    ctx.fillRect(paddel.positX, (canvas.height - paddel.height), paddel.width, paddel.height);
+
+    if (gameOver === true) {
+
+        //Affichage du Game Over
+        ctx.font = "bold 80px Verdana";
+        ctx.fillStyle = "SlateBlue";
+        ctx.fillText("Game Over !", (canvas.width - ctx.measureText("Game Over !").width) / 2, canvas.height / 2);
+
+        //Arrêt de l'animation
+        cancelAnimationFrame(stopanimate);
+
+        ctx.beginPath();
+        ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+        ctx.fillStyle = circle.color;
+        ctx.fill();
+
+        ctx.fillStyle = paddel.color;
+        ctx.fillRect(paddel.positX, (canvas.height - paddel.height), paddel.width, paddel.height);
+    };
+
+};
+
+
+function initPositions() {
+    paddel.positX = (canvas.width - paddel.width) / 2;
+
+    circle.x = 450;
+    circle.y = 660;
+}
+
+
+
+
+
+
+
+
+
+
+
+
